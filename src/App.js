0f2dd13e21e9d46f0e6ce1e145e8491689226ee2
GeorgeP225 <gpayne@greeningaustralia.org.au>
2021-12-19 22:36:22 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,198 +1,185 @@
-import React from 'react';
-import UniqueId from 'react-html-id';
-import { ToDo } from './ToDo/ToDo';
-import { Completed } from './Completed/Completed';
-import ls from 'local-storage';
-import './App.css';
-import Plus from './assets/graphics/Plus.svg';
+import React, {useState} from 'react'
+import { ToDo } from './ToDo/ToDo'
+import { Completed } from './Completed/Completed'
+import ls from 'local-storage'
+import './App.css'
+import Plus from './assets/graphics/Plus.svg'
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    UniqueId.enableUniqueIds(this);
-    this.state = {
-      todos: ls.get('toDos') || [
-        { title: 'note one', id: '231r' },
-        { title: 'note two', id: 'efef' },
-        { title: 'note three', id: 'sd09s' }
-      ],
-      message: '',
-      completed: ls.get('completeds') || [
-        { title: '1', id: 'grhwg' },
-        { title: '2', id: '9joi' }
-      ],
-      showForm: false,
-      showCompleted: false
-    };
-  }
+export const App = () => {
+  const [state, setState] = useState({
+    todos: ls.get('toDos') || [
+      { title: 'note one', id: '231r' },
+      { title: 'note two', id: 'efef' },
+      { title: 'note three', id: 'sd09s' }
+    ],
+    message: '',
+    completed: ls.get('completeds') || [
+      { title: '1', id: 'grhwg' },
+      { title: '2', id: '9joi' }
+    ],
+    showForm: false,
+    showCompleted: false
+  })
 
-  async addItem(e) {
-    e.preventDefault();
-    let newItem = { title: this.newItem.value };
-    const isOnTheList = this.state.todos.includes(newItem);
+  const [newItem, setNewItem] = useState()
+
+  const addItem = async (e) => {
+    e.preventDefault()
+    const isOnTheList = state.todos.includes(newItem)
+    const addedItem = { title: newItem, id: Math.random() }
     if (isOnTheList) {
-      this.setState({
+      setState({...state,
         message: 'This To-do is already on the list.'
-      });
+      })
     } else {
-      newItem =
-        { title: this.newItem.value, id: this.nextUniqueId() } &&
-        newItem !== '' &&
-        this.setState({
-          todos: [...this.state.todos, newItem],
-          message: 'Added entry to to-do list'
-        });
-      await localStorage.setItem('toDos', JSON.stringify(this.state.todos));
+      setState({...state,
+        todos: [...state.todos, addedItem],
+        message: 'Added entry to to-do list'
+      })
+      await localStorage.setItem('toDos', JSON.stringify(state.todos))
     }
-    this.addForm.reset();
-    await localStorage.setItem('toDos', JSON.stringify(this.state.todos));
-    const store = await localStorage.getItem('toDos');
-    const newTodos = JSON.parse(store);
+    await localStorage.setItem('toDos', JSON.stringify(state.todos))
+    const store = await localStorage.getItem('toDos')
+    const newTodos = JSON.parse(store)
   }
 
-  async completedItem(item) {
-    const newTodos = this.state.todos.filter(todo => {
-      return todo !== item;
-    });
+  const completedItem = async (item) => {
+    const newTodos = state.todos.filter(todo => {
+      return todo !== item
+    })
     if (1 === 1) {
-      this.setState({
-        completed: [...this.state.completed, item],
+      setState({...state,
+        completed: [...state.completed, item],
         message: 'Added to completed list',
         todos: [...newTodos]
-      });
+      })
       await localStorage.setItem(
         'completeds',
-        JSON.stringify(this.state.completed)
-      );
-      await localStorage.setItem('toDos', JSON.stringify([...newTodos]));
+        JSON.stringify(state.completed)
+      )
+      await localStorage.setItem('toDos', JSON.stringify([...newTodos]))
     }
     await localStorage.setItem(
       'completeds',
-      JSON.stringify(this.state.completed)
-    );
-    await localStorage.setItem('toDos', JSON.stringify([...newTodos]));
-    const completedStore = await localStorage.getItem('completeds');
-    // this.setState({
-    //   completedLS: [...completedStore]
-    // });
-    console.log('Here is get ');
-    console.log(JSON.parse(completedStore));
+      JSON.stringify(state.completed)
+    )
+    await localStorage.setItem('toDos', JSON.stringify([...newTodos]))
+    const completedStore = await localStorage.getItem('completeds')
   }
-  async removeItem(item) {
-    const newTodos = this.state.completed.filter(todo => {
-      return todo !== item;
-    });
+
+  const removeItem = async (item) => {
+    const newTodos = state.completed.filter(todo => {
+      return todo !== item
+    })
     if (1 === 1) {
-      this.setState({
+      setState({...state,
         completed: [...newTodos],
         message: 'Deleted old to-do'
-      });
-      await localStorage.setItem('completeds', JSON.stringify([...newTodos]));
+      })
+      await localStorage.setItem('completeds', JSON.stringify([...newTodos]))
     }
-    await localStorage.setItem('completeds', JSON.stringify([...newTodos]));
-    const completedRemovals = await localStorage.getItem('completeds');
+    await localStorage.setItem('completeds', JSON.stringify([...newTodos]))
+    const completedRemovals = await localStorage.getItem('completeds')
   }
 
-  toggleInputHandler = () => {
-    const doesShow = this.state.showForm;
-    this.setState({ showForm: !doesShow });
-  };
-  toggleCompletedHandler = () => {
-    const doesShow = this.state.showCompleted;
-    this.setState({ showCompleted: !doesShow });
-  };
+  const toggleInputHandler = () => {
+    const doesShow = state.showForm
+    setState({...state, showForm: !doesShow })
+  }
 
-  render() {
-    const { todos, completed, message } = this.state;
-    return (
-      <div>
-        <div className="header">
-          <h1 className="banner">Your To-Do's</h1>
-          <div className="instructions">
-            Click the plus to add one. Click the todo itself to move lists or
-            remove. Click 'Show Completed'.
+  const toggleCompletedHandler = () => {
+    const doesShow = state.showCompleted
+    setState({...state, showCompleted: !doesShow })
+  }
+
+  const { todos, completed, message } = state
+
+  return (
+    <div>
+      <div className="header">
+        <h1 className="banner">Your To-Do's</h1>
+        <div className="instructions">
+          Click the plus to add one. Click the todo itself to move lists or
+          remove. Click 'Show Completed'.
+        </div>
+        <p className="msg">{message}</p>
+        <img
+          src={Plus}
+          alt="plus_clickme_show_input_form"
+          onClick={() => toggleInputHandler()}
+          className="plus"
+        />
+
+        {state.showForm === true ? (
+          <div className="inputForm">
+            <form
+              onSubmit={(e) => addItem(e)}
+            >
+              <div>
+                <input
+                  type="text"
+                  placeholder="Type To-do Here"
+                  id="newItemInput"
+                  className="input"
+                  value={newItem}
+                  onChange={e => setNewItem(e.target.value)}
+                />
+                <br />
+                <br />
+                <button className="button" type="submit">
+                  Add
+                </button>
+                <br />
+                <br />
+              </div>
+            </form>
           </div>
-          <p className="msg">{message}</p>
-          <img
-            src={Plus}
-            alt="plus_clickme_show_input_form"
-            onClick={this.toggleInputHandler}
-            className="plus"
-          />
-
-          {this.state.showForm === true ? (
-            <div className="inputForm">
-              <form
-                ref={input => (this.addForm = input)}
-                onSubmit={e => {
-                  this.addItem(e);
-                }}
-              >
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Type To-do Here"
-                    id="newItemInput"
-                    ref={input => (this.newItem = input)}
-                    className="input"
-                  />
-                  <br />
-                  <br />
-                  <button className="button" type="submit">
-                    Add
-                  </button>
-                  <br />
-                  <br />
-                </div>
-              </form>
-            </div>
-          ) : null}
-        </div>
-
-        <div className="block">
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  {todos.map(item => (
-                    <ToDo
-                      click={() => this.completedItem(item)}
-                      title={item.title}
-                      key={item.id}
-                    />
-                  ))}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <button
-            className="completedBtn"
-            onClick={this.toggleCompletedHandler}
-          >
-            Show Completed
-          </button>
-          {this.state.showCompleted === true ? (
-            <div>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>
-                      {completed.map(item => (
-                        <Completed
-                          click={() => this.removeItem(item)}
-                          title={item.title}
-                          key={item.id}
-                        />
-                      ))}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          ) : null}
-        </div>
+        ) : null}
       </div>
-    );
-  }
+
+      <div className="block">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                {todos.map(item => (
+                  <ToDo
+                    click={() => completedItem(item)}
+                    title={item.title}
+                    key={item.id}
+                  />
+                ))}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button
+          className="completedBtn"
+          onClick={() => toggleCompletedHandler()}
+        >
+          Show Completed
+        </button>
+        {state.showCompleted === true ? (
+          <div>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    {completed.map(item => (
+                      <Completed
+                        click={() => removeItem(item)}
+                        title={item.title}
+                        key={item.id}
+                      />
+                    ))}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
 }
-export default App;
+export default App
